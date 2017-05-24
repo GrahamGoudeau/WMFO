07f583457623a25232680a761b08e02f617cf2e6
Graham Goudeau <grahamgoudeau@gmail.com>
2017-05-24 14:00:04 -0400

    Request module deauthorizes user if 401 response

--- client/src/ts/request.ts
+++ client/src/ts/request.ts
@@ -1,3 +1,5 @@
+import { browserHistory } from "react-router";
+import { AuthState } from "./authState";
 declare var $: any;
 
 export default class WMFORequest {
@@ -43,20 +45,38 @@ export default class WMFORequest {
     }
 
     async POST(url: string, data?: any, headers?: { [headers: string]: string }) {
-        return await $.ajax({
-            url: url,
-            method: 'POST',
-            headers: Object.assign({}, this.globalHeaders, headers ? headers : {}),
-            dataType: 'json',
-            data: data ? data : {}
-        });
+        try {
+            return await $.ajax({
+                url: url,
+                method: 'POST',
+                headers: Object.assign({}, this.globalHeaders, headers ? headers : {}),
+                dataType: 'json',
+                data: data ? data : {}
+            });
+        } catch (e) {
+            if (e.status === 401) {
+                await AuthState.getInstance().deauthorize();
+                browserHistory.push('/');
+                throw e;
+            }
+            throw e;
+        }
     }
 
     async GET(url: string, headers?: { [headers: string]: string }) {
-        return await $.ajax({
-            url: url,
-            method: 'GET',
-            headers: Object.assign({}, this.globalHeaders, headers ? headers : {})
-        });
+        try {
+            return await $.ajax({
+                url: url,
+                method: 'GET',
+                headers: Object.assign({}, this.globalHeaders, headers ? headers : {})
+            });
+        } catch (e) {
+            if (e.status === 401) {
+                await AuthState.getInstance().deauthorize();
+                browserHistory.push('/');
+                throw e;
+            }
+            throw e;
+        }
     }
 }
