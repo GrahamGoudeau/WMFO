709ae3ba3e9e3c8a5ee1ecbc82cb5a1bb1f70f96
Graham Goudeau <grahamgoudeau@gmail.com>
2017-05-30 01:02:52 -0400

    Remove unused step in db setup script

--- server/db/setupDb.sh
+++ server/db/setupDb.sh
@@ -9,14 +9,6 @@ case $yn in
     * ) echo "Exiting..."; exit 1;;
 esac
 
-read -p "Create wmfo_webmaster user? [y/n/drop] " yn
-case $yn in
-    [Yy]* ) echo "Creating user..."; createuser wmfo_webmaster -d -P; createdb wmfo_webmaster;;
-    drop ) dropdb wmfo_webmaster; dropdb wmfo_db; dropuser wmfo_webmaster; exit 0 ;;
-    * ) ;;
-esac
-if [ "$?" -ne 0 ] ; then exit 1;
-fi
 echo
 #if [ "$?" -eq 1 ] ; then exit 1
 #fi
