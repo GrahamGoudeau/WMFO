fda099f714ee7117a4027136723d2795947c82f7
Graham Goudeau <grahamgoudeau@gmail.com>
2017-06-03 18:37:19 -0400

    Fix print statements and add one more check to setupDb.sh

--- server/db/setupDb.sh
+++ server/db/setupDb.sh
@@ -2,7 +2,6 @@
 
 if [ "$#" -ne 1 ] ; then echo "Usage: ./setup.sh [db url (use 'localhost' for dev), provide full URI otherwise]"; exit 1
 fi
-echo
 read -p "WARNING this script will drop the entire database. Continue? [y/n] " yn
 case $yn in
     [Yy]* ) ;;
@@ -15,5 +14,11 @@ echo
 if [ "$1" == "localhost" ] ; then
     psql -h $1 -f initLocal.sql
 else
+    printf "\n*****\t"
+    read -p "WARNING Not working locally- are you sure you want to continue? [y/n] " yn
+    case $yn in
+        [Yy]* ) ;;
+        * ) exit 1;;
+    esac
     psql $1 -f init.sql
 fi
