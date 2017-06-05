56fdf9456355fde98d0af10964b28d1c81765f0d
Graham Goudeau <grahamgoudeau@gmail.com>
2017-06-04 23:10:52 -0400

    Finish registration email method

--- server/utils/emailer.ts
+++ server/utils/emailer.ts
@@ -4,13 +4,13 @@ import * as handlebars from "handlebars";
 import Logger from "./logger";
 import Config from "./config";
 import Maybe from "./maybe";
-import * as SparkPost from "sparkpost";
+const NodeMailer = require("nodemailer");
 const CONFIG: Config = Config.getInstance();
 
 export abstract class Emailer {
     private static INSTANCE: Emailer = null;
     protected abstract log: Logger;
-    abstract async registerNotification(recipients: { firstName: string, email: string, url: string }[]): Promise<string[]>;
+    abstract async registerNotification(recipients: { email: string, url: string }[]): Promise<string[]>;
 
     static getInstance() {
         if (this.INSTANCE == null) {
@@ -19,7 +19,7 @@ export abstract class Emailer {
             if (isProd || isMailDebug) {
                 this.INSTANCE = new ProdEmailer();
             } else {
-                this.INSTANCE = new DisabledEmailer();
+                this.INSTANCE = new DisabledEmailer()
             }
         }
         return this.INSTANCE;
@@ -28,7 +28,14 @@ export abstract class Emailer {
 
 class ProdEmailer extends Emailer {
     protected readonly log: Logger = new Logger("production-emailer");
-    private readonly sparkPostClient: SparkPost = new SparkPost(CONFIG.getStringConfig('SPARKPOST_API_KEY'));
+    private readonly mailClient: any = NodeMailer.createTransport({
+        host: "smtp.gmail.com",
+        secure: true,
+        auth: {
+            user: CONFIG.getStringConfig("MAIL_USER"),
+            pass: CONFIG.getStringConfig("MAIL_PASS"),
+        }
+    });
     private readonly FROM_ADDRESS: string = 'do-not-reply@wmfo.org';
     private readonly DOMAIN_NAME: string = CONFIG.getStringConfig('DOMAIN_NAME');
     private readonly TEMPLATE_DIR: string = `${__dirname}/../templates/`;
@@ -40,32 +47,48 @@ class ProdEmailer extends Emailer {
         registerNotification: this.compileFromTemplateSource('registerNotification'),
     };
 
-    async registerNotification(recipients: { firstName: string, email: string, url: string }[]): Promise<string[]> {
+    // returns a list of the email addresses that failed to send
+    private async sendMultipleMail(mailOptions: { from: string, to: string, subject: string, html: string }[]): Promise<string[]> {
         const self = this;
-        const failedArrayPromises = recipients.map(recipient => {
-            return this.sparkPostClient.transmissions.send({
-                content: {
-                    from: this.FROM_ADDRESS,
-                    subject: 'Welcome to the WMFO DJ Portal',
-                    html: this.templates.registerNotification({
-                        firstName: recipient.firstName,
-                        url: recipient.url,
-                    }),
-                },
-                recipients: [
-                    { address: recipient.email }
-                ]
-            }).then(data => {
-                console.log('data');
-                if (data.results.total_accepted_recipients !== 1) {
-                    return recipient.email;
-                }
-                return null;
+
+        // send an email after a delay to avoid rate limiting
+        const delay = 1000 * 30;
+        const promises = mailOptions.map((option, index) => {
+            const thisDelay = delay * index;
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    self.log.INFO('Sending to', option.to, ', remaining:', mailOptions.map(opt => opt.to).slice(index + 1));
+                    self.mailClient.sendMail(option, (error: any, info: any) => {
+                        if (error) {
+                            self.log.ERROR('Mail send failed:', error);
+                            resolve(option.to);
+                        }
+                        resolve(null);
+                    });
+                }, thisDelay);
             });
         });
-        return Promise.all(failedArrayPromises)
-            .then(data => data.filter(s => s != null))
-            .catch(e => { self.log.ERROR('mail error experienced', e); throw new Error('mail error'); } );
+        return Promise.all(promises) as Promise<string[]>;
+    }
+
+    async registerNotification(recipients: { email: string, url: string }[]): Promise<string[]> {
+        const self = this;
+        const optionsArr = recipients.map(recipient => {
+            const mailOptions = {
+                from: CONFIG.getStringConfig("MAIL_USER"),
+                to: recipient.email,
+                subject: "Welcome to the WMFO DJ Portal",
+                html: self.templates.registerNotification({
+                    url: recipient.url
+                }),
+            };
+            return mailOptions;
+        });
+        const failedEmails: string[] = await this.sendMultipleMail(optionsArr);
+        if (failedEmails.length > 0) {
+            this.log.ERROR('Failed to send emails to', failedEmails);
+        }
+        return failedEmails;
     }
 
     constructor() {
@@ -76,7 +99,7 @@ class ProdEmailer extends Emailer {
 
 class DisabledEmailer extends Emailer {
     protected readonly log: Logger = new Logger("disabled-emailer");
-    async registerNotification(recipients: { firstName: string, email: string, url: string }[]): Promise<string[]> {
+    async registerNotification(recipients: { email: string, url: string }[]): Promise<string[]> {
         return [];
     }
 
